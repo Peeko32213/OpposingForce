fcc9f60f48edb5fa56f6d36745b10d1b36c119b9
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-06-16 22:13:26 -0600

    stuff

--- src/main/java/com/unusualmodding/opposing_force/client/renderer/HauntedToolRenderer.java
+++ src/main/java/com/unusualmodding/opposing_force/client/renderer/HauntedToolRenderer.java
@@ -27,15 +27,25 @@ public class HauntedToolRenderer extends EntityRenderer<HauntedTool> {
         ItemStack itemStack = entity.getItemStack();
         ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
         BakedModel bakedModel = renderer.getModel(itemStack, entity.level(), null, 0);
+
         float ageInTicks = entity.tickCount + partialTicks;
+        float strikeProgress = entity.getStrikeTime(partialTicks);
 
         poseStack.pushPose();
         poseStack.translate(0, 0.05F + Math.sin(ageInTicks * 0.1F) * 0.15F, 0);
+
+        if (entity.isAttacking()) {
+        }
+
         poseStack.mulPose(Axis.YN.rotationDegrees(Mth.lerp(partialTicks, entity.yRotO, entity.getYRot()) - 180.0F));
         poseStack.mulPose(Axis.XN.rotationDegrees(Mth.lerp(partialTicks, entity.xRotO, entity.getXRot())));
-        Minecraft.getInstance().getItemRenderer().render(itemStack, ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, false, poseStack, source, i, OverlayTexture.NO_OVERLAY, bakedModel);
-        poseStack.popPose();
 
+        poseStack.translate(0, strikeProgress * 0.1F, strikeProgress * 0.2F);
+        poseStack.mulPose(Axis.XN.rotationDegrees(strikeProgress * 90F));
+
+        Minecraft.getInstance().getItemRenderer().render(itemStack, ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, false, poseStack, source, i, OverlayTexture.NO_OVERLAY, bakedModel);
+
+        poseStack.popPose();
         super.render(entity, entityYaw, partialTicks, poseStack, source, i);
     }
 
--- src/main/java/com/unusualmodding/opposing_force/entity/HauntedTool.java
+++ src/main/java/com/unusualmodding/opposing_force/entity/HauntedTool.java
@@ -9,6 +9,7 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -17,11 +18,14 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.util.AirAndWaterRandomPos;
 import net.minecraft.world.entity.ai.util.HoverRandomPos;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
@@ -34,10 +38,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
+import java.util.Objects;
 
 public class HauntedTool extends Monster {
 
-    private static final EntityDataAccessor<ItemStack> ITEMSTACK = SynchedEntityData.defineId(HauntedTool.class, EntityDataSerializers.ITEM_STACK);
+    private static final EntityDataAccessor<ItemStack> ITEM_STACK = SynchedEntityData.defineId(HauntedTool.class, EntityDataSerializers.ITEM_STACK);
+    private static final EntityDataAccessor<Boolean> ATTACKING = SynchedEntityData.defineId(HauntedTool.class, EntityDataSerializers.BOOLEAN);
+
+    private int strikeTime = 0;
+    private int prevStrikeTime;
 
     @Override
     protected @NotNull PathNavigation createNavigation(Level pLevel) {
@@ -63,11 +72,14 @@ public class HauntedTool extends Monster {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 4.0D).add(Attributes.FLYING_SPEED, 0.9F).add(Attributes.MOVEMENT_SPEED, 0.45F);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 4.0D).add(Attributes.FLYING_SPEED, 0.7F).add(Attributes.MOVEMENT_SPEED, 0.45F).add(Attributes.ATTACK_DAMAGE, 5.0F);
     }
 
     protected void registerGoals() {
+        this.goalSelector.addGoal(1, new HauntedToolAttackGoal(this));
         this.goalSelector.addGoal(2, new HauntedToolWanderGoal(this));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     @Override
@@ -84,12 +96,14 @@ public class HauntedTool extends Monster {
 
     public void tick() {
         super.tick();
+        prevStrikeTime = strikeTime;
     }
 
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
-        this.entityData.define(ITEMSTACK, new ItemStack(Items.DIAMOND_SWORD));
+        this.entityData.define(ITEM_STACK, new ItemStack(Items.DIAMOND_SWORD));
+        this.entityData.define(ATTACKING, false);
     }
 
     @Override
@@ -100,6 +114,7 @@ public class HauntedTool extends Monster {
             this.getItemStack().save(stackTag);
             compoundTag.put("HauntedItem", stackTag);
         }
+        compoundTag.putBoolean("Attacking", this.isAttacking());
     }
 
     @Override
@@ -108,14 +123,27 @@ public class HauntedTool extends Monster {
         if (compoundTag.contains("HauntedItem")) {
             this.setItemStack(ItemStack.of(compoundTag.getCompound("HauntedItem")));
         }
+        this.setAttacking(compoundTag.getBoolean("Attacking"));
     }
 
     public ItemStack getItemStack() {
-        return this.entityData.get(ITEMSTACK);
+        return this.entityData.get(ITEM_STACK);
     }
 
     public void setItemStack(ItemStack item) {
-        this.entityData.set(ITEMSTACK, item);
+        this.entityData.set(ITEM_STACK, item);
+    }
+
+    public boolean isAttacking() {
+        return this.entityData.get(ATTACKING);
+    }
+
+    public void setAttacking(boolean attacking) {
+        this.entityData.set(ATTACKING, attacking);
+    }
+
+    public float getStrikeTime(float partialTick) {
+        return (float) prevStrikeTime + (strikeTime - prevStrikeTime) * partialTick;
     }
 
     public double getDamageForItem(ItemStack itemStack) {
@@ -131,7 +159,7 @@ public class HauntedTool extends Monster {
     }
 
     // goals
-    private class HauntedToolWanderGoal extends Goal {
+    private static class HauntedToolWanderGoal extends Goal {
 
         private final HauntedTool tool;
 
@@ -191,8 +219,72 @@ public class HauntedTool extends Monster {
             Vec3 vec32;
             vec32 = this.tool.getViewVector(0.0F);
 
-            Vec3 vec33 = HoverRandomPos.getPos(this.tool, 8, 7, vec32.x, vec32.z, 1.5707964F, 3, 1);
-            return vec33 != null ? vec33 : AirAndWaterRandomPos.getPos(this.tool, 8, 4, -2, vec32.x, vec32.z, 1.5707963705062866);
+            Vec3 vec33 = HoverRandomPos.getPos(this.tool, 6, 5, vec32.x, vec32.z, 1.5707964F, 3, 1);
+            return vec33 != null ? vec33 : AirAndWaterRandomPos.getPos(this.tool, 6, 4, -2, vec32.x, vec32.z, 1.5707963705062866);
+        }
+    }
+
+    private static class HauntedToolAttackGoal extends Goal {
+
+        protected HauntedTool tool;
+
+        public HauntedToolAttackGoal(HauntedTool tool) {
+            this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
+            this.tool = tool;
+        }
+
+        public void start() {
+            this.tool.setAttacking(false);
+            this.tool.strikeTime = 0;
+        }
+
+        public void stop() {
+            this.tool.setAttacking(false);
+        }
+
+        public boolean canUse() {
+            return this.tool.getTarget() != null && this.tool.getTarget().isAlive();
+        }
+
+        public void tick() {
+            LivingEntity target = this.tool.getTarget();
+
+            if (target != null) {
+                this.tool.lookAt(this.tool.getTarget(), 30F, 30F);
+                this.tool.getLookControl().setLookAt(this.tool.getTarget(), 30F, 30F);
+                double distance = this.tool.distanceToSqr(target.getX(), target.getY(), target.getZ());
+
+                boolean attacking = this.tool.isAttacking();
+
+                if (attacking) {
+                    tickAttack();
+                } else {
+                    this.tool.getNavigation().moveTo(target, 1.0D);
+                    if (distance <= 5) {
+                        this.tool.setAttacking(true);
+                    }
+                }
+            }
+        }
+
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
+
+        protected void tickAttack() {
+            this.tool.strikeTime++;
+            LivingEntity target = this.tool.getTarget();
+
+            if (this.tool.strikeTime == 4) {
+                if (this.tool.distanceTo(Objects.requireNonNull(target)) < 1.25F) {
+                    this.tool.doHurtTarget(target);
+                    this.tool.swing(InteractionHand.MAIN_HAND);
+                }
+            }
+            if (this.tool.strikeTime >= 6) {
+                this.tool.strikeTime = 0;
+                this.tool.setAttacking(false);
+            }
         }
     }
 }
