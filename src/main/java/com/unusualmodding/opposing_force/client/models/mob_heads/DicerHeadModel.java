546f69ef9d58c316aa3e90629820a5a0e8a0e619
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-07-05 22:43:25 -0600

    Update DicerHeadModel.java

--- src/main/java/com/unusualmodding/opposing_force/client/models/mob_heads/DicerHeadModel.java
+++ src/main/java/com/unusualmodding/opposing_force/client/models/mob_heads/DicerHeadModel.java
@@ -27,7 +27,7 @@ public class DicerHeadModel extends MobHeadModelBase {
                 .texOffs(0, 16).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 0).addBox(-1.0F, -3.0F, -6.0F, 2.0F, 5.0F, 2.0F, new CubeDeformation(0.0F))
                 .texOffs(24, 22).addBox(-1.0F, -12.0F, -5.0F, 2.0F, 6.0F, 10.0F, new CubeDeformation(0.0F))
-                .texOffs(25, 9).addBox(-5.0F, -6.0F, -5.0F, 10.0F, 2.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+                .texOffs(25, 9).addBox(-5.0F, -6.0F, -5.0F, 10.0F, 2.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.ZERO);
 
         return LayerDefinition.create(meshdefinition, 128, 128);
     }
@@ -39,7 +39,6 @@ public class DicerHeadModel extends MobHeadModelBase {
         this.Head.y = 0;
     }
 
-    @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int i, int i1, float v, float v1, float v2, float v3) {
         this.root.render(poseStack, vertexConsumer, i, i1, v, v1, v2, v3);
     }
