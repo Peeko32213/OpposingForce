677ecb2613d16894a28dce5f2582084f7834f5a0
Scouter456 <markveenstra14@gmail.com>
2025-11-11 19:25:10 +0100

    -deepwoven doesnt cause vibrations

--- src/main/java/com/unusualmodding/opposing_force/mixins/EntityMixin.java
+++ src/main/java/com/unusualmodding/opposing_force/mixins/EntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
@@ -33,4 +35,23 @@ public abstract class EntityMixin {
             }
         }
     }
+
+    @Inject(method = "dampensVibrations", at = @At("RETURN"), cancellable = true)
+    private void opposingforce$dampensVibrations(CallbackInfoReturnable<Boolean> cir) {
+        Entity entity = (Entity) (Object) this;
+
+        if (!(entity instanceof LivingEntity living)) return;
+
+        if (isWearingFullDeepwovenSet(living)) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    private static boolean isWearingFullDeepwovenSet(LivingEntity entity) {
+        return entity.getItemBySlot(EquipmentSlot.HEAD).is(OPItems.DEEPWOVEN_HAT.get())
+                && entity.getItemBySlot(EquipmentSlot.CHEST).is(OPItems.DEEPWOVEN_TUNIC.get())
+                && entity.getItemBySlot(EquipmentSlot.LEGS).is(OPItems.DEEPWOVEN_PANTS.get())
+                && entity.getItemBySlot(EquipmentSlot.FEET).is(OPItems.DEEPWOVEN_BOOTS.get());
+    }
+
 }
