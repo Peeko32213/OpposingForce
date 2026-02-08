00ba000347ebad27f7d063daad5586bdfaef1989
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2026-02-08 15:50:05 -0700

    deepwoven update

--- src/main/java/com/unusualmodding/opposing_force/client/models/armor/DeepwovenArmorModel.java
+++ src/main/java/com/unusualmodding/opposing_force/client/models/armor/DeepwovenArmorModel.java
@@ -26,44 +26,39 @@ public class DeepwovenArmorModel extends HumanoidModel<LivingEntity> {
 		PartDefinition leftArm = root.getChild("left_arm");
 		PartDefinition rightArm = root.getChild("right_arm");
 
-		head.addOrReplaceChild("hat", CubeListBuilder.create()
-				.texOffs(42, 115).addBox(-5.0F, -2.0F, -5.0F, 10.0F, 3.0F, 10.0F, new CubeDeformation(0.01F))
-				.texOffs(54, 103).addBox(-3.0F, -3.25F, -4.0F, 6.0F, 4.0F, 0.0F, new CubeDeformation(0.1F))
-				.texOffs(42, 103).addBox(-2.0F, -16.0F, -2.0F, 4.0F, 7.0F, 4.0F, new CubeDeformation(0.3F))
-				.texOffs(72, 101).addBox(-7.0F, -9.0F, -7.0F, 14.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
-				.texOffs(42, 114).addBox(-7.0F, -10.0F, -7.0F, 14.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
-				.texOffs(42, 114).addBox(-7.0F, -10.0F, 7.0F, 14.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
-				.texOffs(42, 100).addBox(7.0F, -10.0F, -7.0F, 0.0F, 1.0F, 14.0F, new CubeDeformation(0.0F))
-				.texOffs(42, 100).addBox(-7.0F, -10.0F, -7.0F, 0.0F, 1.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.ZERO);
+        head.addOrReplaceChild("hat", CubeListBuilder.create()
+                .texOffs(42, 115).addBox(-5.0F, -2.5F, -5.0F, 10.0F, 3.0F, 10.0F, new CubeDeformation(0.6F))
+                .texOffs(42, 103).addBox(-2.0F, -17.0F, -2.0F, 4.0F, 7.0F, 4.0F, new CubeDeformation(0.6F))
+                .texOffs(72, 101).addBox(-7.0F, -9.0F, -7.0F, 14.0F, 2.0F, 14.0F, new CubeDeformation(0.5F)), PartPose.ZERO);
 
-		body.addOrReplaceChild("tunic", CubeListBuilder.create()
-				.texOffs(58, 74).addBox(-5.0F, -0.3F, -2.5F, 10.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
-				.texOffs(74, 85).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.2F)), PartPose.ZERO);
+        body.addOrReplaceChild("tunic", CubeListBuilder.create()
+                .texOffs(58, 74).addBox(-5.0F, -0.8F, -2.5F, 10.0F, 6.0F, 5.0F, new CubeDeformation(0.5F))
+                .texOffs(74, 85).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.5F)), PartPose.ZERO);
 
-		rightArm.addOrReplaceChild("right_sleeve", CubeListBuilder.create()
-				.texOffs(58, 85).mirror().addBox(-3.0F, -2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.4F)).mirror(false)
-				.texOffs(58, 93).addBox(-3.0F, 4.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.4F))
-				.texOffs(42, 80).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.32F)), PartPose.ZERO);
+        rightArm.addOrReplaceChild("right_sleeve", CubeListBuilder.create()
+                .texOffs(58, 85).mirror().addBox(-3.0F, -2.25F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.8F)).mirror(false)
+                .texOffs(59, 104).addBox(-3.0F, 4.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.8F))
+                .texOffs(42, 80).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.6F)), PartPose.ZERO);
 
-		leftArm.addOrReplaceChild("left_sleeve", CubeListBuilder.create()
-				.texOffs(58, 85).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.4F))
-				.texOffs(58, 93).addBox(-1.0F, 4.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.4F))
-				.texOffs(42, 80).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.32F)).mirror(false), PartPose.ZERO);
+        leftArm.addOrReplaceChild("left_sleeve", CubeListBuilder.create()
+                .texOffs(58, 85).addBox(-1.0F, -2.25F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(0.8F))
+                .texOffs(58, 93).addBox(-1.0F, 4.0F, -2.0F, 4.0F, 6.0F, 4.0F, new CubeDeformation(0.8F))
+                .texOffs(42, 80).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.6F)).mirror(false), PartPose.ZERO);
 
-		leftLeg.addOrReplaceChild("left_pants", CubeListBuilder.create()
-				.texOffs(42, 67).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.01F)).mirror(false)
-				.texOffs(42, 96).mirror().addBox(-1.55F, 0.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.25F)).mirror(false), PartPose.ZERO);
+        rightLeg.addOrReplaceChild("right_pants", CubeListBuilder.create()
+                .texOffs(42, 96).addBox(-2.45F, 0.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.6F))
+                .texOffs(42, 67).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)), PartPose.ZERO);
 
-		rightLeg.addOrReplaceChild("right_pants", CubeListBuilder.create()
-				.texOffs(42, 96).addBox(-2.45F, 0.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.25F))
-				.texOffs(42, 67).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.01F)), PartPose.ZERO);
+        rightLeg.addOrReplaceChild("right_boot", CubeListBuilder.create()
+                .texOffs(58, 67).mirror().addBox(-2.1F, 9.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.5F)).mirror(false), PartPose.ZERO);
 
-		rightLeg.addOrReplaceChild("right_boot", CubeListBuilder.create()
-				.texOffs(58, 67).mirror().addBox(-2.1F, 9.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.3F)).mirror(false), PartPose.ZERO);
+        leftLeg.addOrReplaceChild("left_pants", CubeListBuilder.create().texOffs(42, 67).mirror().addBox(-2.0F, 0.0F, -2.0F, 4.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)).mirror(false)
+                .texOffs(42, 96).mirror().addBox(-1.55F, 0.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.6F)).mirror(false), PartPose.ZERO);
 
-		leftLeg.addOrReplaceChild("left_boot", CubeListBuilder.create()
-				.texOffs(58, 67).addBox(-1.85F, 9.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.3F)), PartPose.ZERO);
 
-		return LayerDefinition.create(meshdefinition, 128, 128);
+        leftLeg.addOrReplaceChild("left_boot", CubeListBuilder.create()
+                .texOffs(58, 67).addBox(-1.85F, 9.0F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.5F)), PartPose.ZERO);
+
+        return LayerDefinition.create(meshdefinition, 128, 128);
 	}
 }
